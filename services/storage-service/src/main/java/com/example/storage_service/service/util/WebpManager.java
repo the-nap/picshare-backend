308380cb95002e7f5475ed8e7c49a5cd170ac114
Leonardo Napoli <leonardo.npl.02@gmail.com>
2026-01-13 16:03:54 +0100

    class refactoring

--- services/storage-service/src/main/java/com/example/storage_service/service/util/WebpManager.java
+++ services/storage-service/src/main/java/com/example/storage_service/service/util/WebpManager.java
@@ -4,7 +4,7 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.Iterator;
+import java.util.List;
 
 import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
@@ -14,58 +14,47 @@ import javax.imageio.stream.ImageOutputStream;
 
 import org.imgscalr.Scalr;
 
-import com.example.storage_service.service.exceptions.StorageException;
-
 public class WebpManager {
 
-  public static void manage(InputStream in, OutputStream outMedia, OutputStream outThumbnail) throws IOException{
-    BufferedImage tempImage = ImageIO.read(in);
-    if(tempImage == null)
-      throw new StorageException("Image not available");
-    saveToDisk(tempImage, outMedia, 0.85f);
-    tempImage.flush();
-    BufferedImage thumbnail = resize(tempImage, 300);
-    saveToDisk(thumbnail, outThumbnail, 0.45f);
-    thumbnail.flush();
+  public static void toWebp(InputStream originalImage, OutputStream[] outputs) throws IOException{
+    BufferedImage original = ImageIO.read(originalImage);
+    BufferedImage thumbnail = Scalr.resize(original, Scalr.Method.SPEED, 300);
+
+    ImageWriter writer = ImageIO.getImageWritersByFormatName("webp").next();
+    
+    try(ImageOutputStream ios = ImageIO.createImageOutputStream(outputs[0])) {
+      writer.setOutput(ios);
+
+      //set compression
+      ImageWriteParam param = writer.getDefaultWriteParam();
+      param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+      param.setCompressionQuality(0.85f);
+
+      //write media to stream
+      writer.write(null, new IIOImage(original, null, null), param);
+    } finally {
+      writer.reset();
+    }
+
+    try(ImageOutputStream ios = ImageIO.createImageOutputStream(outputs[1])) {
+      writer.setOutput(ios);
+
+      //set compression
+      ImageWriteParam param = writer.getDefaultWriteParam();
+      param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+      param.setCompressionQuality(0.45f);
+
+      //write media to stream
+      writer.write(null, new IIOImage(thumbnail, null, null), param);
+    } finally {
+      writer.dispose();
+    }
   }
-
-
-  private static BufferedImage resize(BufferedImage base, int width){
-    return Scalr.resize(base, Scalr.Method.BALANCED, Scalr.Mode.AUTOMATIC, width);
+  
+  public static BufferedImage read(InputStream input) throws IOException{
+    BufferedImage image = ImageIO.read(input);
+    if(image == null)
+      throw new IOException("Unsupported Image");
+    return image;
   }
-
-  private static void saveToDisk(BufferedImage image, OutputStream out, float quality) throws IOException{
-    ImageWriter writer = getWriter();
-    ImageWriteParam param = setQuality(writer, quality);
-
-    writeToDisk(out, image, writer, param);
-  }
-
-  private static void writeToDisk(OutputStream out, BufferedImage image, ImageWriter writer, ImageWriteParam param)
-      throws IOException {
-      try(ImageOutputStream ios = ImageIO.createImageOutputStream(out)) {
-        writer.setOutput(ios);
-        writer.write(null, new IIOImage(image, null, null), param);
-      } finally {
-        writer.dispose();
-      }
-  }
-
-  private static ImageWriteParam setQuality(ImageWriter writer, float quality) {
-    ImageWriteParam ret = writer.getDefaultWriteParam();
-    if(!ret.canWriteCompressed())
-      throw new StorageException("Writer not valid");
-    ret.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-    ret.setCompressionQuality(quality);
-    return ret;
-  }
-
-  private static ImageWriter getWriter() {
-    Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName("webp");
-    if(!writers.hasNext())
-      throw new StorageException("Writer not found");
-    return writers.next();
-  }
-
-
 }
