0ab20cd12cf474aa883dc160a09a26112b1dfed6
Leonardo Napoli <leonardo.npl.02@gmail.com>
2026-01-19 19:14:26 +0100

    added logic to get followers and following in service

--- services/user-service/src/main/java/com/picshare/service/UserService.java
+++ services/user-service/src/main/java/com/picshare/service/UserService.java
@@ -1,7 +1,13 @@
 package com.picshare.service;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
+import com.picshare.dto.UserDTO;
+import com.picshare.mapper.UserMapper;
+import com.picshare.repository.ConnectionRepository;
 import com.picshare.repository.UserRepository;
 
 import lombok.AllArgsConstructor;
@@ -10,4 +16,20 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class UserService {
   private final UserRepository userRepository;
+  private final ConnectionRepository connectionRepository;
+  private final UserMapper userMapper;
+
+  public Set<UserDTO> getFollowers(Long id){
+    return connectionRepository.findByFollowed(userRepository.findById(id).get())
+      .stream()
+      .map(connection -> userMapper.toDto(connection.getFollower()))
+      .collect(Collectors.toSet());
+  }
+
+  public Set<UserDTO> getFollowed(Long id){
+    return connectionRepository.findByFollower(userRepository.findById(id).get())
+      .stream()
+      .map(connection -> userMapper.toDto(connection.getFollowed()))
+      .collect(Collectors.toSet());
+  }
 }
