808ee07a800c22f929c5f0462121e66f666fa7d2
Leonardo Napoli <leonardo.npl.02@gmail.com>
2026-01-24 20:20:28 +0100

    added new functionalities

--- services/user-service/src/main/java/com/picshare/service/UserService.java
+++ services/user-service/src/main/java/com/picshare/service/UserService.java
@@ -1,11 +1,14 @@
 package com.picshare.service;
 
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 import com.picshare.dto.UserDTO;
+import com.picshare.entity.ConnectionEntity;
+import com.picshare.entity.UserEntity;
 import com.picshare.mapper.UserMapper;
 import com.picshare.repository.ConnectionRepository;
 import com.picshare.repository.UserRepository;
@@ -31,5 +34,33 @@ public class UserService {
       .stream()
       .map(connection -> userMapper.toDto(connection.getFollowed()))
       .collect(Collectors.toSet());
+  }
+
+  private boolean exist(Long userId, Long toFollowId){
+    if(!userRepository.existsById(userId) || !userRepository.existsById(toFollowId))
+      return false;
+    return true;
+
+  }
+
+  public void follow(Long userId, Long toFollowId){
+    if(!exist(userId, toFollowId))
+      return; //ToDo throw exception
+
+    UserEntity user = userRepository.findById(userId).get();
+    UserEntity toFollow = userRepository.findById(toFollowId).get();
+    connectionRepository.save(new ConnectionEntity(user, toFollow));
+  }
+
+  public void removeFollow(Long userId, Long toFollowId){
+    if(!exist(userId, toFollowId))
+      return; //ToDo throw exception
+
+    UserEntity user = userRepository.findById(userId).get();
+    UserEntity toFollow = userRepository.findById(toFollowId).get();
+    if(!connectionRepository.existsByFollowerAndFollowed(user, toFollow))
+      return; //ToDo throw exception
+
+    connectionRepository.delete(connectionRepository.findByFollowerAndFollowed(user, toFollow));
   }
 }
