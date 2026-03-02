31187366e442df10744027d2f77558a607881b0f
Leonardo Napoli <leonardo.npl.02@gmail.com>
2026-03-02 19:11:25 +0100

    added keycloak uri

--- gateway/src/main/java/com/picshare/gateway/SecurityConfig.java
+++ gateway/src/main/java/com/picshare/gateway/SecurityConfig.java
@@ -2,11 +2,13 @@ package com.picshare.gateway;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,16 +17,21 @@ import lombok.RequiredArgsConstructor;
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
+  @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+  String jwkSetUri;
+
   @Bean
   public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) throws Exception {
-    return http
+    http
       .authorizeExchange((auth) -> auth
           .pathMatchers("/api/user/create").hasAuthority("SCOPE_create:users")
           .anyExchange().authenticated()
           )
-      .cors(withDefaults())
       .oauth2ResourceServer(oauth2 -> oauth2
           .jwt(withDefaults()))
-      .build();
+      .securityContextRepository(NoOpServerSecurityContextRepository.getInstance());
+
+    return http.build();
   }
+
 }
