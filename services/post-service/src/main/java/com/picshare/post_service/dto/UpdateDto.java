56c360d19f4057d303de8b569d053bf80beec025
Leonardo Napoli <leonardo.npl.02@gmail.com>
2026-03-26 16:07:29 +0100

    added methods for search by tag

--- services/post-service/src/main/java/com/picshare/post_service/dto/UpdateDto.java
+++ services/post-service/src/main/java/com/picshare/post_service/dto/UpdateDto.java
@@ -8,6 +8,6 @@ import lombok.Data;
 public class UpdateDto {
 
   private final String userId;
-  private final Long postId;
+  private final String postId;
 
 }
--- services/post-service/src/main/java/com/picshare/post_service/entity/PostEntity.java
+++ services/post-service/src/main/java/com/picshare/post_service/entity/PostEntity.java
@@ -5,13 +5,17 @@ import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -43,7 +47,9 @@ public class PostEntity {
   @NonNull
   private String description;
 
-  @Column(name = "tags")
+  @ElementCollection(targetClass = String.class, fetch = FetchType.LAZY)
+  @CollectionTable(name = "entity_tags", joinColumns = @JoinColumn(name = "entity_id"))
+  @Column(name = "tag")
   private List<String> tags;
   
   @Column(name = "status", nullable = false)
