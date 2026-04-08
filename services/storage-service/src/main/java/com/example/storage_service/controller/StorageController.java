a7856b8ef5fd45d41be2c832bee1fee38b324a7e
Leonardo Napoli <leonardo.npl.02@gmail.com>
2026-04-08 12:23:50 +0200

    refactored controller

--- services/storage-service/src/main/java/com/example/storage_service/controller/StorageController.java
+++ services/storage-service/src/main/java/com/example/storage_service/controller/StorageController.java
@@ -2,7 +2,6 @@ package com.example.storage_service.controller;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Map;
 
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -24,38 +24,39 @@ public class StorageController {
 
   private final StorageService service;
   
-  @PostMapping("/store")
-  public ResponseEntity<?> store(
-      @RequestParam("file") MultipartFile file, 
-      @RequestParam("id") Long id){
+  @PostMapping("/store/{id}")
+  public ResponseEntity<Void> store(@RequestParam("file") MultipartFile file, @PathVariable String id){
 
     try(InputStream is = file.getInputStream()){
-      service.store(is, id.toString());
+      service.store(is, id);
     } catch(IOException e) {
       throw new StorageException("Error while reading resource");
     }
-    return ResponseEntity
-      .ok(
-          Map.ofEntries(
-            Map.entry("imageId", id))
-         );
+    return ResponseEntity.ok().build();
   }
+
+  @PostMapping("/avatar/{id}")
+  public ResponseEntity<Void> storeAvatar(@RequestPart(value = "image") MultipartFile image, @PathVariable String id){
+    try(InputStream is = image.getInputStream()){
+      service.storeAvatar(is, id);
+    }catch (IOException e) {
+      throw new StorageException("Error while reading resource");
+    }
+    return ResponseEntity.ok().build();
+  }
+
   
   @GetMapping("/media/{id}")
-  public ResponseEntity<Resource> serveMedia(
-      @PathVariable Long id) {
-
-      Resource resource = service.serveMedia(id.toString());
-      return ResponseEntity.ok(resource);
-
+  public ResponseEntity<Resource> serveMedia(@PathVariable String id) {
+      return ResponseEntity.ok()
+        .body(
+          service.serveMedia(id));
   }
 
   @GetMapping("/thumbnail/{id}")
-  public ResponseEntity<Resource> serveThumbnail
-  (@PathVariable Long id){
-
-    Resource resource = service.serveThumbnail(id.toString());
-    return ResponseEntity.ok(resource);
-
+  public ResponseEntity<Resource> serveThumbnail(@PathVariable String id){
+    return ResponseEntity.ok()
+      .body(
+          service.serveThumbnail(id));
   }
 }
