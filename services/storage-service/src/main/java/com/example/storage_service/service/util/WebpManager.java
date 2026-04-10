5dca9b1c423a07d98167323f8a38712abca60b50
Leonardo Napoli <leonardo.npl.02@gmail.com>
2026-04-10 19:38:17 +0200

    refactor

--- services/storage-service/src/main/java/com/example/storage_service/service/util/WebpManager.java
+++ services/storage-service/src/main/java/com/example/storage_service/service/util/WebpManager.java
@@ -4,7 +4,6 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.List;
 
 import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
@@ -16,13 +15,44 @@ import org.imgscalr.Scalr;
 
 public class WebpManager {
 
-  public static void toWebp(InputStream originalImage, OutputStream[] outputs) throws IOException{
+  public static void toWebp(InputStream originalImage, OutputStream originalOutput, OutputStream previewOutput) throws IOException{
     BufferedImage original = ImageIO.read(originalImage);
-    BufferedImage thumbnail = Scalr.resize(original, Scalr.Method.SPEED, 300);
+    BufferedImage preview = Scalr.resize(original, Scalr.Method.SPEED, 300);
 
     ImageWriter writer = ImageIO.getImageWritersByFormatName("webp").next();
     
-    try(ImageOutputStream ios = ImageIO.createImageOutputStream(outputs[0])) {
+    writeOriginal(originalOutput, original, writer);
+
+    writePreview(previewOutput, preview, writer);
+  }
+
+  public static void toWebp(InputStream inputStream, OutputStream out) throws IOException{
+    BufferedImage media = ImageIO.read(inputStream);
+    
+    ImageWriter writer = ImageIO.getImageWritersByFormatName("webp").next();
+    
+    writeOriginal(out, media, writer);
+  }
+
+  private static void writePreview(OutputStream output, BufferedImage preview, ImageWriter writer)
+      throws IOException {
+    try(ImageOutputStream ios = ImageIO.createImageOutputStream(output)) {
+      writer.setOutput(ios);
+
+      //set compression
+      ImageWriteParam param = writer.getDefaultWriteParam();
+      param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+      param.setCompressionQuality(0.45f);
+
+      //write media to stream
+      writer.write(null, new IIOImage(preview, null, null), param);
+    } finally {
+      writer.dispose();
+    }
+  }
+
+  private static void writeOriginal(OutputStream output, BufferedImage original, ImageWriter writer) throws IOException {
+    try(ImageOutputStream ios = ImageIO.createImageOutputStream(output)) {
       writer.setOutput(ios);
 
       //set compression
@@ -34,20 +64,6 @@ public class WebpManager {
       writer.write(null, new IIOImage(original, null, null), param);
     } finally {
       writer.reset();
-    }
-
-    try(ImageOutputStream ios = ImageIO.createImageOutputStream(outputs[1])) {
-      writer.setOutput(ios);
-
-      //set compression
-      ImageWriteParam param = writer.getDefaultWriteParam();
-      param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-      param.setCompressionQuality(0.45f);
-
-      //write media to stream
-      writer.write(null, new IIOImage(thumbnail, null, null), param);
-    } finally {
-      writer.dispose();
     }
   }
   
