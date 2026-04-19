ad4b6d1437fbbc0b5ffd7b08e2d61c3c152f23df
Leonardo Napoli <leonardo.npl.02@proton.me>
2026-04-19 21:01:36 +0200

    better error handling

--- services/post-service/src/main/java/com/picshare/post_service/controller/PostControllerAdvice.java
+++ services/post-service/src/main/java/com/picshare/post_service/controller/PostControllerAdvice.java
@@ -1,17 +1,18 @@
 package com.picshare.post_service.controller;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class PostControllerAdvice {
 
   @ExceptionHandler(Exception.class)
   @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-  String genericHandler(Exception se) {
-    return se.getMessage();
+  ResponseEntity<String> genericHandler(Exception se) {
+    return ResponseEntity.status(500).body(se.getMessage());
   }
 
 }
