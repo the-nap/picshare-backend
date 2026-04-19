2a02e3b2a3aeb2813beeb15937835df02455c11b
Leonardo Napoli <leonardo.npl.02@proton.me>
2026-04-19 21:05:29 +0200

    changed dependency for webp images

--- services/storage-service/src/main/java/com/example/storage_service/service/util/WebpManager.java
+++ services/storage-service/src/main/java/com/example/storage_service/service/util/WebpManager.java
@@ -4,6 +4,7 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Iterator;
 
 import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
@@ -12,26 +13,25 @@ import javax.imageio.ImageWriter;
 import javax.imageio.stream.ImageOutputStream;
 
 import org.imgscalr.Scalr;
+import org.springframework.stereotype.Component;
 
+import com.luciad.imageio.webp.WebPWriteParam;
+
+@Component
 public class WebpManager {
 
   public static void toWebp(InputStream originalImage, OutputStream originalOutput, OutputStream previewOutput) throws IOException{
     BufferedImage original = ImageIO.read(originalImage);
+
     BufferedImage preview = Scalr.resize(original, Scalr.Method.SPEED, 300);
 
     ImageWriter writer = ImageIO.getImageWritersByFormatName("webp").next();
     
-    writeOriginal(originalOutput, original, writer);
+    if(originalOutput != null)
+      writeOriginal(originalOutput, original, writer);
 
-    writePreview(previewOutput, preview, writer);
-  }
-
-  public static void toWebp(InputStream inputStream, OutputStream out) throws IOException{
-    BufferedImage media = ImageIO.read(inputStream);
-    
-    ImageWriter writer = ImageIO.getImageWritersByFormatName("webp").next();
-    
-    writeOriginal(out, media, writer);
+    if(previewOutput != null)
+      writePreview(previewOutput, preview, writer);
   }
 
   private static void writePreview(OutputStream output, BufferedImage preview, ImageWriter writer)
@@ -40,8 +40,9 @@ public class WebpManager {
       writer.setOutput(ios);
 
       //set compression
-      ImageWriteParam param = writer.getDefaultWriteParam();
+      WebPWriteParam param = new WebPWriteParam(writer.getLocale());
       param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+      param.setCompressionType(param.getCompressionTypes()[WebPWriteParam.LOSSLESS_COMPRESSION]);
       param.setCompressionQuality(0.45f);
 
       //write media to stream
@@ -56,8 +57,9 @@ public class WebpManager {
       writer.setOutput(ios);
 
       //set compression
-      ImageWriteParam param = writer.getDefaultWriteParam();
+      WebPWriteParam param = new WebPWriteParam(writer.getLocale());
       param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+      param.setCompressionType(param.getCompressionTypes()[WebPWriteParam.LOSSLESS_COMPRESSION]);
       param.setCompressionQuality(0.85f);
 
       //write media to stream
