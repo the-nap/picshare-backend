7a3556b6ba07dd73ab23fb33affc77ae6d8f5d27
Leonardo Napoli <leonardo.npl.02@proton.me>
2026-04-25 13:08:39 +0200

    fixed repository tyte mismatch

--- services/post-service/src/main/java/com/picshare/post_service/service/repository/PostRepository.java
+++ services/post-service/src/main/java/com/picshare/post_service/service/repository/PostRepository.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Repository;
 import com.picshare.post_service.service.entity.PostEntity;
 
 @Repository
-public interface PostRepository extends JpaRepository<PostEntity, Long> {
+public interface PostRepository extends JpaRepository<PostEntity, String> {
 
   Optional<PostEntity> findById(String id);
 
