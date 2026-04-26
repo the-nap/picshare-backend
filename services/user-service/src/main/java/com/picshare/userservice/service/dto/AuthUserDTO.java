3a386d3d02acc24116b6b1a134fc1f9a767b7877
Leonardo Napoli <leonardo.npl.02@proton.me>
2026-04-26 16:54:56 +0200

    fix to previous commit

--- services/user-service/src/main/java/com/picshare/userservice/service/dto/AuthUserDTO.java
+++ services/user-service/src/main/java/com/picshare/userservice/service/dto/AuthUserDTO.java
@@ -1,23 +1,21 @@
 package com.picshare.userservice.service.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class AuthUserDTO {
   
-  @NotBlank(message = "Id cannot be blank")
   String id;
 
-  @NotBlank(message = "Email cannot be blank")
   String email;
 
-  @NotBlank(message = "Username cannot be blank")
   String username;
 
 }
