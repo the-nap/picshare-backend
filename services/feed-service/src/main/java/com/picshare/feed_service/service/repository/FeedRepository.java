1aaec072ed6193e111e2c6e770bb9b35c63f2c9d
Leonardo Napoli <leonardo.npl.02@proton.me>
2026-04-29 16:15:58 +0200

    added overload to repository

--- services/feed-service/src/main/java/com/picshare/feed_service/service/repository/FeedRepository.java
+++ services/feed-service/src/main/java/com/picshare/feed_service/service/repository/FeedRepository.java
@@ -9,16 +9,31 @@ import org.springframework.data.util.Streamable;
 import org.springframework.stereotype.Repository;
 
 import com.picshare.feed_service.service.entity.FeedEntity;
+import com.picshare.feed_service.service.entity.FeedStatus;
 
 @Repository
 public interface FeedRepository extends CrudRepository<FeedEntity, String>{
 
-  Streamable<FeedEntity> findByUserId(String userId, Pageable pageable);
+  Streamable<FeedEntity> findByUserId(String userId, FeedStatus status, Pageable pageable);
 
-  Streamable<FeedEntity> findByPostId(String postId, Pageable pageable);
+  Streamable<FeedEntity> findByPostId(String postId, FeedStatus status, Pageable pageable);
 
-  Optional<FeedEntity> findByUserIdAndPostId(String userId, String postId);
+  Optional<FeedEntity> findByUserIdAndPostId(String userId, String postId, FeedStatus status);
 
   void deleteAllByTimestampBefore(Date date);
 
+  void deleteAllByStatus(FeedStatus status);
+
+
+  default Streamable<FeedEntity> findByUserId(String userId, Pageable pageable){
+    return this.findByUserId(userId, FeedStatus.REGULAR, pageable);
+  }
+
+  default Streamable<FeedEntity> findByPostId(String postId, Pageable pageable){
+    return this.findByUserId(postId, FeedStatus.REGULAR, pageable);
+  }
+
+  default Optional<FeedEntity> findByUserIdAndPostId(String userId, String postId){
+    return this.findByUserIdAndPostId(userId, postId, FeedStatus.REGULAR);
+  }
 }
