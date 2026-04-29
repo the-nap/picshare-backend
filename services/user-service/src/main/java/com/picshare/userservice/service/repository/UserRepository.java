5a4eadfe816a49e216075cc0d5e62048f3703a58
Leonardo Napoli <leonardo.npl.02@proton.me>
2026-04-29 18:30:09 +0200

    fixed repository to include status

--- services/user-service/src/main/java/com/picshare/userservice/service/repository/UserRepository.java
+++ services/user-service/src/main/java/com/picshare/userservice/service/repository/UserRepository.java
@@ -9,47 +9,69 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.picshare.userservice.service.entity.UserEntity;
+import com.picshare.userservice.service.entity.UserStatus;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, String> {
 
-  Optional<UserEntity> findById(String id);
+  Optional<UserEntity> findByIdAndStatus(String id, UserStatus status);
 
-  Optional<UserEntity> findByUsername(String username);
+  Optional<UserEntity> findByUsernameAndStatus(String username, UserStatus status);
 
-  boolean existsByUsername(String username);
+  boolean existsByUsernameAndStatus(String username, UserStatus status);
 
-  Optional<UserEntity> findByEmail(String email);
+  Optional<UserEntity> findByEmailAndStatus(String email, UserStatus status);
 
-  void deleteById(String id);
+  void deleteByIdAndStatus(String id, UserStatus status);
 
   @Query(
-    value = "SELECT * FROM users u WHERE u.username LIKE %?1% ORDER BY creation_date OFFSET ?2 LIMIT ?3",
+    value = "SELECT * FROM users u WHERE u.username LIKE %?1% AND u.status = 'REGULAR' ORDER BY creation_date OFFSET ?2 LIMIT ?3", 
     nativeQuery = true)
   List<UserEntity> searchByUsername(String username, Integer offset, Integer max);
 
   @Query(
-    value = "SELECT * FROM users u WHERE u.email LIKE %?1% ORDER BY creation_date OFFSET ?2 LIMIT ?3",
+    value = "SELECT * FROM users u WHERE u.email LIKE %?1% AND u.status = 'REGULAR' ORDER BY creation_date OFFSET ?2 LIMIT ?3",
     nativeQuery = true)
   List<UserEntity> searchByEmail(String email, Integer offset, Integer max);
 
   @Query(
-    value = "SELECT * FROM users u WHERE u.email LIKE %?1% OR u.username LIKE %?1% ORDER BY creation_date OFFSET ?2 LIMIT ?3",
+    value = "SELECT * FROM users u WHERE u.email LIKE %?1% OR u.username LIKE %?1% AND u.status = 'REGULAR' ORDER BY creation_date OFFSET ?2 LIMIT ?3",
     nativeQuery = true)
   List<UserEntity> searchByEmailOrUsername(String toSearch, Integer offset, Integer max);
 
   @Query(
-    value = "SELECT * FROM users u WHERE u.username LIKE %?1%",
+    value = "SELECT * FROM users u WHERE u.username LIKE %?1% AND u.status = 'REGULAR'",
     nativeQuery = true)
   Set<UserEntity> countByUsername(String username);
 
   @Query(
-    value = "SELECT * FROM users u WHERE u.email LIKE %?1%",
+    value = "SELECT * FROM users u WHERE u.email LIKE %?1% AND u.status = 'REGULAR'",
     nativeQuery = true)
   Set<UserEntity> countByEmail(String email);
 
   @Query(
-    value = "SELECT * FROM users u ORDER BY creation_date OFFSET ?1 LIMIT ?2",
+    value = "SELECT * FROM users u AND u.status = 'REGULAR' ORDER BY creation_date OFFSET ?1 LIMIT ?2",
     nativeQuery = true)
   Set<UserEntity> getAll(Integer first, Integer max);
+
+
+  default Optional<UserEntity> findById(String id){
+    return this.findByIdAndStatus(id, UserStatus.REGULAR);
+  }
+
+  default Optional<UserEntity> findByUsername(String username){
+    return this.findByUsernameAndStatus(username, UserStatus.REGULAR);
+  }
+
+  default boolean existsByUsername(String username){
+    return this.existsByUsernameAndStatus(username, UserStatus.REGULAR);
+  }
+
+  default Optional<UserEntity> findByEmail(String email){
+    return this.findByEmailAndStatus(email, UserStatus.REGULAR);
+  }
+
+  default void deleteById(String id){
+    this.deleteByIdAndStatus(id, UserStatus.REGULAR);
+  }
 }
